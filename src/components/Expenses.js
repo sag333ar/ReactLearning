7af56e0cf6139d33c180ccbca927d489a9211eb7
Sagar Kothari <sagar.kothari@datarobot.com>
2021-05-25 22:06:07 +0530

    added css for expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
 
 const Expenses = ({expenses}) => {
   return (
-    <div>
+    <div className="expenses">
       {expenses.map((e, i) => (
         <ExpenseItem date={e.date} key={i} amount={e.amount} title={e.title} />
       ))}

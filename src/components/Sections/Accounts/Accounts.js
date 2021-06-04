d541e7dde78cf219efcb3178e39651e5867c2ce3
Sagar Kothari <sagar.kothari@datarobot.com>
2021-06-04 16:02:44 +0530

    table for accounts

--- src/components/Sections/Accounts/Accounts.js
+++ src/components/Sections/Accounts/Accounts.js
@@ -1,7 +1,35 @@
 import React from "react";
+import cssClasses from "./Accounts.module.css";
 
 const Accounts = () => {
-  return <h1>This is Accounts page</h1>;
+  const accounts = [
+    { name: "Personal", icon: "🏡"} , 
+    { name: "Business", icon: "💼"},
+    { name: "Travel", icon: "✈️"},
+  ];
+  return (
+    <table className={cssClasses.table}>
+      <thead className={cssClasses.row}>
+        <tr>
+          <th className={cssClasses.index}>#</th>
+          <th className={cssClasses.title}>Name</th>
+          <th className={cssClasses.icon}>Icon</th>
+        </tr>
+      </thead>
+      <tbody>
+      {accounts.map((account) => {
+        const index = accounts.indexOf(account) + 1;
+        return (
+          <tr key={index} className={cssClasses.row}>
+            <td className={cssClasses.index}>{index}</td>
+            <td className={cssClasses.title}>{account.name}</td>
+            <td className={cssClasses.icon}>{account.icon}</td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  );
 };
 
 export default Accounts;
